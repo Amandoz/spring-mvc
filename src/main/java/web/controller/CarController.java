93f6a86300da44249693a998ae0ec64f52b0182c
amand <amandoz@list.ru>
2020-08-27 22:13:59 +0300

    1

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import service.Service;
+import service.ServiceImpl;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -13,13 +15,10 @@ import java.util.List;
 
 @Controller
 public class CarController {
-
+    Service service = new ServiceImpl();
     @GetMapping("/cars")
-    public String printWelcome(@RequestParam(name = "locale", required = false, defaultValue = "ru")String locale, ModelMap model) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("bmw", 1, 20200.5));
-        cars.add(new Car("bmw", 2, 20300.5));
-        cars.add(new Car("bmw", 3, 4000.5));
+    public String printCar(@RequestParam(name = "locale", required = false, defaultValue = "ru")String locale, ModelMap model) {
+        List<Car> cars = service.getCars();
         model.addAttribute("cars", cars);
         model.addAttribute("locale", locale);
         return "cars";
